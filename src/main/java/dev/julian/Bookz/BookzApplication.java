e919685c2ef68335cb42f2de420df27ac7726b84
Julian Wan <julianwan@Julians-MBP-21.realtek>
2025-01-05 14:58:36 +0800

    First commit

---
+++ src/main/java/dev/julian/Bookz/BookzApplication.java
@@ -0,0 +1,13 @@
+package dev.julian.Bookz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookzApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/julian/Bookz/BookControllerTest.java
@@ -0,0 +1,2 @@
+package dev.julian.Bookz;public class BookControllerTest {
+}
