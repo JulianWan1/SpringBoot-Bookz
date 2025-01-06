c3901800f9dbe97ca66f2e1efd8d08de0cb3e9e0
Julian Wan <julianwan@Julians-MBP-21.realtek>
2025-01-07 01:53:30 +0800

    Set post put and delete tests and APIs

--- src/main/java/dev/julian/Bookz/book/BookController.java
+++ src/main/java/dev/julian/Bookz/book/BookController.java
@@ -1,9 +1,8 @@
 package dev.julian.Bookz.book;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -12,19 +11,48 @@ import java.util.Optional;
 @RequestMapping("/api/books")
 public class BookController {
 
-    private final BookRepository bookRepository;
+    private final BookRepository repository;
 
-    BookController(BookRepository bookRepository) {
-        this.bookRepository = bookRepository;
+    BookController(BookRepository repository) {
+        this.repository = repository;
     }
 
     @GetMapping("")
     List<Book> findAll() {
-        return bookRepository.findAll();
+        return repository.findAll();
     }
 
     @GetMapping("/{id}")
     Optional<Book> findById(@PathVariable Integer id) {
-        return bookRepository.findById(id);
+        return Optional.ofNullable(repository.findById(id)
+                .orElseThrow(BookNotFoundException::new));
+    }
+    
+    @PostMapping("")
+    @ResponseStatus(HttpStatus.CREATED)
+    Book save(@RequestBody @Valid Book book) {
+        return repository.save(book);
+    }
+
+    @PutMapping("/{id}")
+    Book update(@PathVariable Integer id, @RequestBody @Valid Book book) {
+        Optional<Book> existing = repository.findById(id);
+
+        if (existing.isPresent()) {
+           Book updatedBook = new Book(existing.get().id(),
+                   book.title(),
+                   book.description(),
+                   existing.get().version());
+
+           return repository.save(updatedBook);
+        } else {
+            throw new BookNotFoundException();
+        }
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("/{id}")
+    void delete(@PathVariable Integer id) {
+        repository.deleteById(id);
     }
 }
--- src/test/java/dev/julian/Bookz/BookControllerTest.java
+++ src/test/java/dev/julian/Bookz/BookControllerTest.java
@@ -3,6 +3,7 @@ package dev.julian.Bookz;
 import dev.julian.Bookz.book.Book;
 import dev.julian.Bookz.book.BookController;
 import dev.julian.Bookz.book.BookRepository;
+import dev.julian.Bookz.book.BookNotFoundException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,15 +11,13 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-import static java.lang.StringTemplate.STR;
 import static org.mockito.Mockito.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -88,5 +87,89 @@ public class BookControllerTest {
         mockMvc.perform(get("/api/books/1"))
                 .andExpect(status().isOk())
                 .andExpect(content().json(json));
+    }
+
+    @Test
+    void shouldNotFindWhenGivenInvalidId() throws Exception {
+        when(repository.findById(999)).thenThrow(BookNotFoundException.class);
+
+        mockMvc.perform(get("/api/books/999"))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    void shouldCreateNewBookWhenGivenValidBook() throws Exception {
+        Book book = new Book(3, "LARPing for dummies", "Learn to live your fantasies, in reality!", null);
+
+        String json = STR."""
+                {
+                    "id":\{book.id()},
+                    "title":"\{book.title()}",
+                    "description":"\{book.description()}",
+                    "version": null
+                }
+                """;
+
+        when(repository.save(book)).thenReturn(book);
+
+        mockMvc.perform(post("/api/books")
+                .contentType("application/json")
+                .content(json))
+                .andExpect(status().isCreated())
+                .andExpect(content().json(json));
+    }
+
+    @Test
+    void shouldNotCreateBookWhenBookIsInvalid() throws Exception {
+        Book book = new Book(3, "", "", null);
+
+        String json = STR."""
+                {
+                    "id":\{book.id()},
+                    "title":"\{book.title()}",
+                    "description":"\{book.description()}",
+                    "version": null
+                }
+                """;
+
+        when(repository.save(book)).thenReturn(book);
+
+        mockMvc.perform(post("/api/books")
+                .contentType("application/json")
+                .content(json))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    void shouldUpdatePostWhenGivenValidBook() throws Exception {
+        Book updated = new Book(1,"Hello World V2","Updated version", null);
+
+        when(repository.findById(1)).thenReturn(Optional.of(books.getFirst()));
+        when(repository.save(updated)).thenReturn(updated);
+
+        String requestBody = STR."""
+                {
+                    "id":\{updated.id()},
+                    "title":"\{updated.title()}",
+                    "description":"\{updated.description()}",
+                    "version": \{updated.version()}
+                }
+                """;
+
+        mockMvc.perform(put("/api/books/1")
+                .contentType("application/json")
+                .content(requestBody))
+                .andExpect(status().isOk());
+
+    }
+
+    @Test
+    void shouldDeleteBookWhenGivenValidId() throws Exception {
+        doNothing().when(repository).deleteById(1);
+
+        mockMvc.perform(delete("/api/books/1"))
+                .andExpect(status().isNoContent());
+
+        verify(repository, times(1)).deleteById(1);
     }
 }
